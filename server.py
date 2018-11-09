71dc26bc266bf74a987ad20b67072f42a286e6a5
Costanza Volpini <costanza.volpini@gmail.com>
2018-11-09 15:07:24 +0100

    camera works

--- server.py
+++ server.py
@@ -1,6 +1,6 @@
 from flask import Flask, render_template, request, jsonify
-import time
 import cv2
+import time
 
 app = Flask(__name__)
 
@@ -11,6 +11,7 @@ app = Flask(__name__)
 # url_for('static', filename='main.css')
 # url_for('static', filename='responsive.css')
 # url_for('static', filename='jquery.fancybox.css')
+
 
 @app.route('/')
 def hello_world():
@@ -33,9 +34,19 @@ def get_emotions_path():
 # export FLASK_APP=server.py && export FLASK_ENV=development && flask run
 
 
-camera_port = 0
-camera = cv2.VideoCapture(camera_port)
-time.sleep(0.1)  # If you don't wait, the image will be dark
-return_value, image = camera.read()
-cv2.imwrite("opencv.png", image)
-del(camera)  # so that others can use the camera as soon as possible
+# camera_port = 0
+# camera = cv2.VideoCapture(camera_port)
+# time.sleep(0.1)  # If you don't wait, the image will be dark
+# return_value, image = camera.read()
+# cv2.imwrite("opencv.png", image)
+# del(camera)  # so that others can use the camera as soon as possible
+
+@app.route('/experiment')
+def get_camera():
+    camera_port = 0
+    camera = cv2.VideoCapture(camera_port)
+    time.sleep(0.1)
+    return_value, image = camera.read()
+    cv2.imwrite("user.png", image)
+    del camera
+    return "ok"
