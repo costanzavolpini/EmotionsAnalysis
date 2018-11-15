0423b565b8cd64330a51a647438c3fd23a2c9314
Costanza Volpini <costanza.volpini@gmail.com>
2018-11-15 16:07:02 +0100

    added Skander part

--- static/js/main.js
+++ static/js/main.js
@@ -20,7 +20,7 @@ $(document).ready(function () {
 	// Flushed
 	$("input[id=flushed]").on('input change', getValRangeFlushed);
 
-	function getValRangeFlushed(e){
+	function getValRangeFlushed(e) {
 		value_flushed = e.target.value;
 		$("#flushedValue").text(value_flushed + "%");
 	}
@@ -28,7 +28,7 @@ $(document).ready(function () {
 	// Frown
 	$("input[id=frown]").on('input change', getValRangeFrown);
 
-	function getValRangeFrown(e){
+	function getValRangeFrown(e) {
 		value_frown = e.target.value;
 		$("#frownValue").text(value_frown + "%");
 	}
@@ -36,7 +36,7 @@ $(document).ready(function () {
 	// Grin
 	$("input[id=grin]").on('input change', getValRangeGrin);
 
-	function getValRangeGrin(e){
+	function getValRangeGrin(e) {
 		value_grin = e.target.value;
 		$("#grinValue").text(value_grin + "%");
 	}
@@ -44,49 +44,51 @@ $(document).ready(function () {
 	// Surprise
 	$("input[id=surprise]").on('input change', getValRangeSurprise);
 
-	function getValRangeSurprise(e){
+	function getValRangeSurprise(e) {
 		value_surprise = e.target.value;
 		$("#surpriseValue").text(value_surprise + "%");
 	}
 
 	var data_emotions_path = {
-		"flushed" : value_flushed,
-		"frown" : value_frown,
-		"grin" : value_grin,
-		"surprise" : value_surprise
+		"flushed": value_flushed,
+		"frown": value_frown,
+		"grin": value_grin,
+		"surprise": value_surprise
 	}
 
 	// Function that handle generate and return of the emotions
-	$('#generatePath').click(function(e) {
+	$('#generatePath').click(function (e) {
 		e.preventDefault();
-        $.ajax({
-            url: '/path',
-            data: JSON.stringify(data_emotions_path),
-            type: 'POST',
-            success: function(response) {
+		$.ajax({
+			url: '/path',
+			data: JSON.stringify(data_emotions_path),
+			type: 'POST',
+			success: function (response) {
 				// Should receive the new path image
 				$("#pathGenerated")[0].src.replace("path", "giorda");
 				var actualPath = $("#pathGenerated")[0].src.replace("path", "giorda")
 				$("#pathGenerated").attr("src", actualPath);
-            },
-            error: function(error) {
-                console.log(error);
-            }
-        });
+			},
+			error: function (error) {
+				console.log(error);
+			}
+		});
 	});
 
 
-	$('#experiment').click(function(e){
+	$('#experiment').click(function (e) {
 		$('#modalVideo').modal('show')
-		navigator.permissions.query({name: 'camera'})
-		.then((permissionObj) => {
-			console.log(permissionObj.state);
-			$.ajax({
-				url: '/experiment',
-				type: 'GET',
-				success: function(response) {
-					// Should receive the new path image
-					your_result = response.scores
+		navigator.permissions.query({
+				name: 'camera'
+			})
+			.then((permissionObj) => {
+				console.log(permissionObj.state);
+				$.ajax({
+					url: '/experiment',
+					type: 'GET',
+					success: function (response) {
+						// Should receive the new path image
+						your_result = response.scores
 
 						var ctxRyour = document.getElementById("yourEmotion").getContext('2d');
 						var your = new Chart(ctxRyour, {
@@ -94,61 +96,59 @@ $(document).ready(function () {
 							data: {
 								labels: ["Anger", "Contempt", "Disgust", "Happiness", "Fear", "Neutral", "Sadness", "Surprise"],
 								datasets: [{
-										label: "YOUR RESULT",
-										data: [your_result.anger * 100, your_result.contempt * 100, your_result.disgust * 100, your_result.fear * 100, your_result.happiness * 100, your_result.neutral * 100, your_result.sadness * 100, your_result.surprise * 100],
-										backgroundColor: [
-											'rgba(105, 0, 132, .2)',
-										],
-										borderColor: [
-											'rgba(200, 99, 132, .7)',
-										],
-										borderWidth: 2
-									}
-								]
+									label: "YOUR RESULT",
+									data: [your_result.anger * 100, your_result.contempt * 100, your_result.disgust * 100, your_result.fear * 100, your_result.happiness * 100, your_result.neutral * 100, your_result.sadness * 100, your_result.surprise * 100],
+									backgroundColor: [
+										'rgba(105, 0, 132, .2)',
+									],
+									borderColor: [
+										'rgba(200, 99, 132, .7)',
+									],
+									borderWidth: 2
+								}]
 							},
 							options: {
 								responsive: true
 							}
 						});
 
-					$('#wrapCluster').removeClass("col-md-6").addClass("col-md-5");
-				},
-				error: function(error) {
-					console.log(error);
-				}
-			});
+						$('#wrapCluster').removeClass("col-md-6").addClass("col-md-5");
+					},
+					error: function (error) {
+						console.log(error);
+					}
+				});
 
-			$('#videoBoulin').contents().find('video').each(function ()
-			{
-				this.currentTime = 0;
-			});
-		})
-		.catch((error) => {
-		 console.log('Got error :', error);
-		})
+				$('#videoBoulin').contents().find('video').each(function () {
+					this.currentTime = 0;
+				});
+			})
+			.catch((error) => {
+				console.log('Got error :', error);
+			})
 	});
 
 
 	// Function that takes a set of images and upload it on microsoft azure returning json
-	$('#saveUpload').click(function(e) {
+	$('#saveUpload').click(function (e) {
 		e.preventDefault();
-});
+	});
 
 
-$('#carouselImages').carousel({
-	interval: 1500
+	$('#carouselImages').carousel({
+		interval: 1500
 	})
 
 
 	// Fancybox
 	// $('.work-box').fancybox();
-$(function () {
-	var ctxR = document.getElementById("radarChart").getContext('2d');
-	var myRadarChart = new Chart(ctxR, {
-		type: 'radar',
-		data: {
-			labels: ["Anger", "Contempt", "Disgust", "Happiness", "Fear", "Neutral", "Sadness", "Surprise"],
-			datasets: [{
+	$(function () {
+		var ctxR = document.getElementById("radarChart").getContext('2d');
+		var myRadarChart = new Chart(ctxR, {
+			type: 'radar',
+			data: {
+				labels: ["Anger", "Contempt", "Disgust", "Happiness", "Fear", "Neutral", "Sadness", "Surprise"],
+				datasets: [{
 					label: "PAINTING NAME",
 					data: [65, 59, 90, 81, 56, 55, 40, 20],
 					backgroundColor: [
@@ -158,50 +158,49 @@ $(function () {
 						'rgba(200, 99, 132, .7)',
 					],
 					borderWidth: 2
-				}
-			]
-		},
-		options: {
-			responsive: true
-		}
+				}]
+			},
+			options: {
+				responsive: true
+			}
+		});
+
+
+		var finalGraph = document.getElementById("emotionFinal").getContext('2d');
+		var myFinalChart = new Chart(finalGraph, {
+			type: 'radar',
+			data: {
+				labels: ["Anger", "Contempt", "Disgust", "Happiness", "Fear", "Neutral", "Sadness", "Surprise"],
+				datasets: [{
+						label: "PAINTING 1",
+						data: [65, 59, 90, 81, 56, 55, 40, 20],
+						backgroundColor: [
+							'rgba(105, 0, 132, .2)',
+						],
+						borderColor: [
+							'rgba(200, 99, 132, .7)',
+						],
+						borderWidth: 2
+					},
+					{
+						label: "PAINTING 2",
+						data: [5, 50, 80, 91, 96, 45, 20, 40],
+						backgroundColor: [
+							'rgba(105, 40, 132, .2)',
+						],
+						borderColor: [
+							'rgba(200, 49, 132, .7)',
+						],
+						borderWidth: 2
+					}
+
+				]
+			},
+			options: {
+				responsive: true
+			}
+		});
 	});
-
-
-	var finalGraph =  document.getElementById("emotionFinal").getContext('2d');
-	var myFinalChart = new Chart(finalGraph, {
-		type: 'radar',
-		data: {
-			labels: ["Anger", "Contempt", "Disgust", "Happiness", "Fear", "Neutral", "Sadness", "Surprise"],
-			datasets: [{
-					label: "PAINTING 1",
-					data: [65, 59, 90, 81, 56, 55, 40, 20],
-					backgroundColor: [
-						'rgba(105, 0, 132, .2)',
-					],
-					borderColor: [
-						'rgba(200, 99, 132, .7)',
-					],
-					borderWidth: 2
-				},
-				{
-					label: "PAINTING 2",
-					data: [5, 50, 80, 91, 96, 45, 20, 40],
-					backgroundColor: [
-						'rgba(105, 40, 132, .2)',
-					],
-					borderColor: [
-						'rgba(200, 49, 132, .7)',
-					],
-					borderWidth: 2
-				}
-
-			]
-		},
-		options: {
-			responsive: true
-		}
-	});
-});
 
 
 	// // Flexslider
@@ -249,10 +248,10 @@ $(function () {
 	$('#selectFloor').on('show.bs.dropdown', function (ev) {
 		// do something…
 		console.log(ev.target)
-		})
+	})
 
-	$(".dropdown-menu li a").click(function(){
-		$(".btn:first-child").html($(this).text()+' <span class="caret"></span>');
+	$(".dropdown-menu li a").click(function () {
+		$(".btn:first-child").html($(this).text() + ' <span class="caret"></span>');
 	});
 
 
@@ -262,7 +261,7 @@ $(function () {
 		var rawFile = new XMLHttpRequest();
 		rawFile.overrideMimeType("application/json");
 		rawFile.open("GET", file, true);
-		rawFile.onreadystatechange = function() {
+		rawFile.onreadystatechange = function () {
 			if (rawFile.readyState === 4 && rawFile.status == "200") {
 				callback(rawFile.responseText);
 			}
@@ -271,42 +270,51 @@ $(function () {
 	}
 
 	//usage:
-	readTextFile("/static/js/database.json", function(text){
+	readTextFile("/static/js/database.json", function (text) {
 		var data = JSON.parse(text);
 		console.log("JSON LOADED");
 
 		console.log(data);
 
 		// TODO: detect if we are in url /diagrams
-		$(jQuery.parseJSON(JSON.stringify(data))).each(function() {
+		$(jQuery.parseJSON(JSON.stringify(data))).each(function () {
 			var id = this.id;
 			var result_arr = this.res;
 			var name = this.name;
 
 			var length = result_arr.length;
 
-			var res = [0,0,0,0,0,0,0,0]
+			var res = [0, 0, 0, 0, 0, 0, 0]
 
 			// find mean
-			for(var i = 0; i < length; i++){
+			for (var i = 0; i < length; i++) {
 				var max = null
 
-				for(var j = 0; j < result_arr[i].length; j++){
-					if((max == null) || (result_arr[i][j] > max)){
-						max = result_arr[i][j]
+				for (var j = 0; j < result_arr[i].length; j++) {
+					if (j != 5) { //remove neutral
+						if ((max == null) || (result_arr[i][j] > max)) {
+							max = result_arr[i][j]
+						}
 					}
 				}
 
-				for(var j = 0; j < result_arr[i].length; j++){
-					var normalized = result_arr[i][j]/max
-					res[j] = res[j] + normalized
+				for (var j = 0; j < result_arr[i].length; j++) {
+					if (j < 5) { //remove neutral
+						var normalized = result_arr[i][j] / max
+						res[j] = res[j] + normalized
+					} else if(j > 5){
+						var normalized = result_arr[i][j] / max
+						res[j-1] = res[j-1] + normalized
+					}
 				}
 			}
 
+			console.log(res)
+
 			// invert happiness with neutral just for a nicer result
-			var temp = res[4]
-			res[4] = res[5]
-			res[5] = temp
+			var temp = res[3]
+			res[3] = res[1]
+			res[1] = temp
 
 			var div = document.createElement("div");
 			div.classList.add("col");
@@ -321,46 +329,47 @@ $(function () {
 			// Add code for chart
 			var ctxR = document.getElementById(id).getContext('2d');
 			var myRadarChart = new Chart(ctxR, {
-			type: 'radar',
-			data: {
-				// anger, contempt, disgust, fear, happiness, neutral, sadness, surprise
-				labels: ["Anger", "Contempt", "Disgust", "Fear", "Neutral", "Happiness", "Sadness", "Surprise"],
-				datasets: [{
-					label: name,
-					data: res,
-					backgroundColor: [
-					'rgba(244, 240, 242, .9)',
-					],
-					borderColor: [
-					'rgba(213, 208, 206, .7)',
-					],
-					pointBackgroundColor: [
-						'rgba(244, 240, 242, .9)',
-					],
-					pointBorderColor: [
-						'rgba(213, 208, 206, .7)',
+				type: 'radar',
+				data: {
+					// anger, contempt, disgust, fear, happiness, neutral, sadness, surprise
+					// invert fear and contempt
+					// fear 3 contempt 1
+					labels: ["Anger", "Fear", "Disgust", "Contempt", "Happiness", "Sadness", "Surprise"],
+					datasets: [{
+						label: name,
+						data: res,
+						backgroundColor: [
+							'rgba(244, 240, 242, .9)',
 						],
-					borderWidth: 1
-				}
-				]
-			},
-			options: {
-				responsive: true,
-				scale: {
-					ticks: {
-					   display: false,
-					   maxTicksLimit: 1
-					}
-				 },
-				//  showScale: false,
-				//  labels: {
-				// 	 fontFamily: "Garamond Black"
-				// }
+						borderColor: [
+							'rgba(213, 208, 206, .7)',
+						],
+						pointBackgroundColor: [
+							'rgba(244, 240, 242, .9)',
+						],
+						pointBorderColor: [
+							'rgba(213, 208, 206, .7)',
+						],
+						borderWidth: 1
+					}]
+				},
+				options: {
+					responsive: true,
+					scale: {
+						ticks: {
+							display: false,
+							maxTicksLimit: 1
+						}
+					},
+					//  showScale: false,
+					//  labels: {
+					// 	 fontFamily: "Garamond Black"
+					// }
 				}
 			});
 
 
-   		});
+		});
 
 	});
 });
